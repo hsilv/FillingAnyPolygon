a08be79acd5db112c5d0ab1cf5bfc055bcd90d15
hsilv <hm.sebastiansilva@gmail.com>
2023-07-10 11:24:22 -0600

    drawLine

--- src/bitmap.h
+++ src/bitmap.h
@@ -3,6 +3,7 @@
 #include "bitmapwrite.h"
 
 void draw_centered_rectangle(size_t width, size_t height);
+void drawLine(float startX, float startY, float endX, float endY, size_t width, size_t height);
 
 void render() {
 
@@ -11,9 +12,6 @@ void render() {
 
     setClearColor(Color(46, 52, 64));
     clear();
-
-    setCurrentColor(Color(66, 135, 245));
-    draw_centered_rectangle(750, 550);
 
     setCurrentColor(Color(255, 0, 0));
     draw_centered_rectangle(150, 150);
@@ -24,7 +22,9 @@ void render() {
     setCurrentColor(Color(0, 0, 255));
     draw_centered_rectangle(50, 50);
 
-    setCurrentColor(Color(255, 0, 0));
+    drawLine(0, 0, 800, 600, framebufferWidth, framebufferHeight);
+    drawLine(0, 600, 800, 0, framebufferWidth, framebufferHeight);
+
     renderBuffer();
 }
 
@@ -37,6 +37,51 @@ void draw_centered_rectangle(size_t width, size_t height) {
         for (float j = startHPoint; j <= startHPoint+width; j++)
         {
             point(Vertex2(j, i), framebufferWidth, framebufferHeight);
+        }
+    }
+}
+
+void drawLine(float startX, float startY, float endX, float endY, size_t width, size_t height) {
+    // Convertir las coordenadas iniciales y finales en enteros
+    int x0 = static_cast<int>(startX);
+    int y0 = static_cast<int>(startY);
+    int x1 = static_cast<int>(endX);
+    int y1 = static_cast<int>(endY);
+
+    // Calcular las diferencias en las coordenadas x e y
+    int dx = std::abs(x1 - x0);
+    int dy = std::abs(y1 - y0);
+
+    // Determinar la dirección de incremento para x e y
+    int sx = (x0 < x1) ? 1 : -1;
+    int sy = (y0 < y1) ? 1 : -1;
+
+    // Calcular el valor de error inicial
+    int error = dx - dy;
+
+    while (true) {
+        // Verificar si las coordenadas están dentro del rango del framebuffer
+        if (x0 >= 0 && x0 < static_cast<int>(width) && y0 >= 0 && y0 < static_cast<int>(height)) {
+            // Dibujar el punto en el framebuffer
+            point(Vertex2{ static_cast<float>(x0), static_cast<float>(y0) }, width, height);
+        }
+
+        // Verificar si se alcanzó el punto final
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
+
+        // Calcular el siguiente punto en la línea
+        int error2 = 2 * error;
+
+        if (error2 > -dy) {
+            error -= dy;
+            x0 += sx;
+        }
+
+        if (error2 < dx) {
+            error += dx;
+            y0 += sy;
         }
     }
 }
